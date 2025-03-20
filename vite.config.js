e7a8b98582defce9dc7c6fe1b0120b0fe50d8b08
JustinStark2022 <starkjustin87@gmail.com>
2025-03-19 22:43:27 -0500

    set up

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   base: './',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
    build: {
     outDir: 'dist',
